628cac59dce3cc9f685f4b74ef1b809f188c4211
Rory Pyman <rory.pyman@gmx.com>
2024-09-12 22:14:25 +0100

    Updated css for new logo

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -9,7 +9,7 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
-            <img src={LogoS} alt="logo" />
+            <img className="sidebar-logo" src={LogoS} alt="logo" />
             <img className='sub-logo' src={LogoSubtitle} alt="name" />
         </Link>
         <nav>
